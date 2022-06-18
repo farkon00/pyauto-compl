4bfaf2c8f9d41157164a738522f882a4c24da198
farkon00 <sammer2016sammer@gmail.com>
2022-06-18 12:56:08 +0300

    Added is_word to PrefixTreeNode

--- autocompl/prefix_tree.py
+++ autocompl/prefix_tree.py
@@ -5,7 +5,16 @@ class PrefixTreeNode(node.Node):
     def __init__(self, value: str):
         super().__init__(value)
         self.value = value
+        self.is_word = False
         self.node_children = {}
+
+    @property
+    def label(self):
+        return f"{self.value}\n{self.is_word}"
+
+    @label.setter
+    def label(self, value):
+        pass
 
     @staticmethod
     def construct_tree(words: list[str]):
@@ -19,5 +28,6 @@ class PrefixTreeNode(node.Node):
                     curr.node_children[char] = PrefixTreeNode(curr_word)
                     curr.children.append(curr.node_children[char])
                 curr = curr.node_children[char]
+            curr.is_word = True
 
         return tree
